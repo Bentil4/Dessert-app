1ebf1d5b9265e4fa123573d486c2e8633bf6ca59
Broderick Nana Bentil <bentilfrederick4@gmail.com>
2026-01-24 23:08:52 +0000

    feat(product-card): enhance product card with image, add to cart button, and quantity management
    - Implement product image display with conditional styling based on cart status
    - Add "Add to Cart" button with click event to manage cart addition
    - Introduce computed properties for cart status and product quantity management
    - Prepare for future quantity control integration

--- src/app/components/product-card/product-card.ts
+++ src/app/components/product-card/product-card.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject, input, output } from '@angular/core';
+import { IProduct } from '../../types/product';
+import { CartService } from '../../services/cart.service';
 
 @Component({
   selector: 'app-product-card',
@@ -7,5 +9,28 @@ import { Component } from '@angular/core';
   styleUrl: './product-card.css',
 })
 export class ProductCard {
+  product = input.required<IProduct>();
+  add = output<IProduct>();
 
+  private cartService = inject(CartService);
+  isInCart = computed(() =>
+    this.cartService.items().some((i) => i.product.name === this.product().name),
+  );
+
+  quantity = computed(
+    () =>
+      this.cartService.items().find((i) => i.product.name === this.product().name)?.quantity ?? 0,
+  );
+
+  addToCart() {
+    this.cartService.addItem(this.product());
+  }
+
+  increase() {
+    this.cartService.increaseQuantity(this.product().name);
+  }
+
+  decrease() {
+    this.cartService.decreaseQuantity(this.product().name);
+  }
 }
