53b8b44a0f8112cb02a10cf62bdb737829822dde
Broderick Nana Bentil <bentilfrederick4@gmail.com>
2026-01-25 23:15:48 +0000

    feat: implement dessert page layout, integrate product list and fully functional cart panel with currency formatting.

--- src/app/components/cart-panel/cart-panel.ts
+++ src/app/components/cart-panel/cart-panel.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CartService } from '../../services/cart.service';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-cart-panel',
-  imports: [],
+  imports: [CurrencyPipe],
   templateUrl: './cart-panel.html',
   styleUrl: './cart-panel.css',
 })
 export class CartPanel {
-
+  cart = inject(CartService);
 }
