1d170dc3f6d5d03511f2a471cad8851b918f04a9
Broderick Nana Bentil <bentilfrederick4@gmail.com>
2026-01-25 23:13:19 +0000

    refactor(routes, services): update routing and enhance products service
    - Refactor routes to use dynamic component loading for Dessert
    - Comment out previous static routes for Dessert and ProductCard
    - Update ProductsService to transform product data with additional properties
    - Implement mapping for product IDs and image URLs in the products service

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,27 @@
 import { Routes } from '@angular/router';
-import { Dessert } from './pages/dessert/dessert';
+// import { Dessert } from './pages/dessert/dessert';
 import { ProductCard } from './components/product-card/product-card';
-
+import { ProductList } from './components/product-list/product-list';
+import { Dessert } from './pages/dessert/dessert';
 export const routes: Routes = [
+  // {
+  //   path: '',
+  //   component: Dessert,
+  //   title: 'Dessert',
+  // },
+  // {
+  //   path: 'Product-Card',
+  //   component: ProductCard,
+  //   title: 'P-card',
+  // },
+  // {
+  //   path: 'Product-List',
+  //   component: ProductList,
+  //   title: 'Pro-list',
+  // },
+
   {
     path: '',
-    component: Dessert,
-    title: 'Dessert',
-  },
-  {
-    path: 'Product-Card',
-    component: ProductCard,
-    title: 'P-card',
+    loadComponent: () => import('./pages/dessert/dessert').then((m) => m.Dessert),
   },
 ];
--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -2,14 +2,23 @@ import { inject, Injectable } from '@angular/core';
 import { IProduct } from '../types/product';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
 })
-class productsService {
+export class ProductsService {
   private http = inject(HttpClient);
 
   getProducts(): Observable<IProduct[]> {
-    return this.http.get<IProduct[]>('./assets/data.data.json');
+    return this.http.get<any[]>('./assets/data/data.json').pipe(
+      map((products) =>
+        products.map((product) => ({
+          ...product,
+          id: String(product.id),
+          imageURL: product.image,
+        })),
+      ),
+    );
   }
 }
