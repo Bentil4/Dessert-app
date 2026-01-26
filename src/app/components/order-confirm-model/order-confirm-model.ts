e89d9780c2a15daeea06e282ff260a76b5d2a733
Broderick Nana Bentil <bentilfrederick4@gmail.com>
2026-01-26 09:33:50 +0000

    feat: Integrate cart service and router for order confirmation display and actions.

--- src/app/components/order-confirm-model/order-confirm-model.ts
+++ src/app/components/order-confirm-model/order-confirm-model.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
+import { CartService } from '../../services/cart.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-order-confirm-model',
@@ -7,5 +9,17 @@ import { Component } from '@angular/core';
   styleUrl: './order-confirm-model.css',
 })
 export class OrderConfirmModel {
+  cartService = inject(CartService);
+  router = inject(Router)
 
+  readonly item = computed(() => this.cartService.items());
+  readonly total = computed(() => this.cartService.totalPrice());
+
+  close():void{
+    this.router.navigateByUrl('/');
+  }
+
+  startNewOrder(): void{
+    this.cartService.clearCart
+  }
 }
