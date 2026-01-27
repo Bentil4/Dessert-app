1afb75375302f4b5bbf497b2c505e3943cbe9455
Broderick Nana Bentil <bentilfrederick4@gmail.com>
2026-01-27 08:46:24 +0000

    feat: Implement order confirmation modal with detailed order summary and update product card UI for in-cart items.

--- src/app/components/order-confirm-model/order-confirm-model.ts
+++ src/app/components/order-confirm-model/order-confirm-model.ts
@@ -1,25 +1,28 @@
-import { Component, computed, inject } from '@angular/core';
+import { Component, computed, inject, output } from '@angular/core';
 import { CartService } from '../../services/cart.service';
 import { Router } from '@angular/router';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-order-confirm-model',
-  imports: [],
+  imports: [CurrencyPipe],
   templateUrl: './order-confirm-model.html',
   styleUrl: './order-confirm-model.css',
 })
 export class OrderConfirmModel {
   cartService = inject(CartService);
-  router = inject(Router)
+  // router = inject(Router)
 
-  readonly item = computed(() => this.cartService.items());
+  readonly items = computed(() => this.cartService.items());
   readonly total = computed(() => this.cartService.totalPrice());
+  readonly close = output<void>();
 
-  close():void{
-    this.router.navigateByUrl('/');
-  }
+  // close():void{
+  //   this.router.navigateByUrl('/');
+  // }
 
-  startNewOrder(): void{
-    this.cartService.clearCart
+  startNewOrder(): void {
+    this.cartService.clearCart();
+    this.close.emit();
   }
 }
