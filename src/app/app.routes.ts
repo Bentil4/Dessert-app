1d934d91dded4797ca99fd8bf5f01b17d4aa8d01
Broderick Nana Bentil <bentilfrederick4@gmail.com>
2026-01-27 09:12:56 +0000

    refactor: Lazy load Dessert component and update confirm route to use OrderConfirmModel.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,34 +1,16 @@
 import { Routes } from '@angular/router';
-// import { Dessert } from './pages/dessert/dessert';
-import { ProductCard } from './components/product-card/product-card';
-import { ProductList } from './components/product-list/product-list';
 import { Dessert } from './pages/dessert/dessert';
 import { PageNotFound } from './pages/page-not-found/page-not-found';
-import { OrderConfirm } from './pages/order-confirm/order-confirm';
-export const routes: Routes = [
-  // {
-  //   path: '',
-  //   component: Dessert,
-  //   title: 'Dessert',
-  // },
-  // {
-  //   path: 'Product-Card',
-  //   component: ProductCard,
-  //   title: 'P-card',
-  // },
-  // {
-  //   path: 'Product-List',
-  //   component: ProductList,
-  //   title: 'Pro-list',
-  // },
+import { OrderConfirmModel } from './components/order-confirm-model/order-confirm-model';
 
+export const routes: Routes = [
   {
     path: '',
     loadComponent: () => import('./pages/dessert/dessert').then((m) => m.Dessert),
   },
   {
-path: "confirm",
-component: OrderConfirm
+    path: 'confirm',
+    component: OrderConfirmModel,
   },
 
   { path: '**', component: PageNotFound, title: '404' },
