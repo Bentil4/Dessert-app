f5b353d1d7e2b34415d5b1e7cce03ad424abd836
Broderick Nana Bentil <bentilfrederick4@gmail.com>
2026-01-28 09:28:11 +0000

    refactor getProduct function in product service class

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,5 +1,5 @@
 import { inject, Injectable } from '@angular/core';
-import { IProduct } from '../types/product';
+import { IProduct, IProductImages } from '../types/product';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
@@ -12,17 +12,23 @@ export class ProductsService {
 
   /**
    * Fetches the list of products from the JSON file.
-   * @returns An observable array of products.
+   * @returns An observable array of products, each with a string id, a string name, a string category, a number price, and an IProductImages object.
    */
   getProducts(): Observable<IProduct[]> {
-    return this.http.get<any[]>('./assets/data/data.json').pipe(
-      map((products) =>
-        products.map((product) => ({
-          ...product,
-          id: String(product.id),
-          imageURL: product.image,
-        })),
-      ),
-    );
+    return this.http
+      .get<
+        { id: string; name: string; category: string; price: number; image: IProductImages }[]
+      >('./assets/data/data.json')
+      .pipe(
+        map((products) =>
+          products.map((product) => ({
+            id: product.id,
+            name: product.name,
+            category: product.category,
+            price: product.price,
+            imageURL: product.image,
+          })),
+        ),
+      );
   }
 }
